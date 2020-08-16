f8499d20731f0c56680e734e09753b06449013fc
Jessica <contact.jessicasanchez@gmail.com>
2020-08-16 14:01:53 -0400

    working redirect

--- server/api/discord.js
+++ server/api/discord.js
@@ -4,12 +4,13 @@ const express = require('express');
 
 const router = express.Router();
 
-const PRACTICE_CLIENT_ID = process.env.CLIENT_ID;
-const PRACTICE_CLIENT_SECRET = process.env.CLIENT_SECRET;
-const redirect = encodeURIComponent('http://localhost:50451/api/discord/callback');
+const PRACTICE_CLIENT_ID = process.env.PRACTICE_CLIENT_ID;
+const PRACTICE_CLIENT_SECRET = process.env.PRACTICE_CLIENT_SECRET;
+const redirect = encodeURIComponent('http://localhost:5000/api/discord/callback');
+
 
 router.get('/login', (request, response) => {
-  response.redirect(`https://discord.com/api/oauth2/authorize?client_id=${PRACTICE_CLIENT_ID}&redirect_uri=${redirect}&response_type=code&scope=guilds%20identify`);
+  response.redirect(`https://discord.com/api/oauth2/authorize?client_id=${PRACTICE_CLIENT_ID}&redirect_uri=${redirect}&response_type=code&scope=guilds`);
 });
 
 module.exports = router;
